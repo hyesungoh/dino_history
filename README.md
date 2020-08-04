99efb3e053676f481247302418a6b897236294a5
hyesungoh <haesungoh414@gmail.com>
2020-08-04 18:39:03 +0900

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dino_history
+#### Django-version == 2.2.4
+#### Python-version == 3.7.4
---
+++ macvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ohyeseong/Documents/django/dino_history/macvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
