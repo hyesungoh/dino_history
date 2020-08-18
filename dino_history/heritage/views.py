a933e4f4c37645045b7410d1eed0033c42f0b061
Youn Dong Yeol <eastheat10@gmail.com>
2020-08-19 00:12:50 +0900

    add forms.py, modified  views(login, singup), urls.py, settings.py at app user

--- dino_history/heritage/views.py
+++ dino_history/heritage/views.py
@@ -6,3 +6,32 @@ def main(request):
 
 def map(request):
     return render(request, 'heritage/map.html')
+
+def signin(request):
+    signin_form = SigninForm()
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            return HttpResponse("로그인 실패. 다시 시도하세요.")
+    else:
+        return render(request, 'insta/signin.html',{'signin_form': signin_form})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = CustomUser.objects.create_user(username=form.cleaned_data['username'],
+            email = form.cleaned_data['email'],
+            password = form.cleaned_data['password'])
+            nickname = form.cleaned_data['nickname'],
+            login(request, new_user)
+            return redirect('main')
+    else:
+        form = UserForm()
+        return render(request, 'insta/signup.html', {'form': form})
--- dino_history/user/views.py
+++ dino_history/user/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student, Problem, Example
+from .forms import SigninForm, UserForm
+
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
 
 # Create your views here.
 def mypage(request):
@@ -9,3 +14,32 @@ def ranking(request):
 
 def problem(request):
     return render(request, 'user/problem.html')
+
+def login(request):
+    signin_form = SigninForm()
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            return HttpResponse("로그인 실패. 다시 시도하세요.")
+    else:
+        return render(request, 'user/login.html',{'signin_form': signin_form})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = CustomUser.objects.create_user(username=form.cleaned_data['username'],
+            email = form.cleaned_data['email'],
+            password = form.cleaned_data['password'])
+            b_date = form.cleaned_data['b_date'],
+            login(request, new_user)
+            return redirect('main')
+    else:
+        form = UserForm()
+        return render(request, 'user/signup.html', {'form': form})
